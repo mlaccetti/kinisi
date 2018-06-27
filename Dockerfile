c0b299943218f1a773738656fb6a3f883a0321d1
Michael Laccetti <michael@laccetti.com>
2018-06-27 17:58:34 -0400

    Working on name resolution

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
 FROM ubuntu:18.04 as build
 
-RUN apt-get update && apt-get dist-upgrade -y && \
-  apt-get install -y build-essential libpcap-dev curl inetutils-ping software-properties-common && \
-  add-apt-repository -y ppa:hnakamur/golang-1.10 && \
-  apt-get update && \
-  apt-get install -y golang-go golang-doc && \
-  mkdir -p /root/go/bin && \
-  curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
+ENV GOPATH="/go"
+ENV PATH="${PATH}:${GOPATH}/bin"
 
 WORKDIR /go/src/github.com/mlaccetti/kinisi
+
+RUN apt-get update && apt-get dist-upgrade -y
+RUN apt-get install -y build-essential libpcap-dev curl inetutils-ping software-properties-common vim-gocomplete
+RUN add-apt-repository -y ppa:hnakamur/golang-1.10
+RUN apt-get update
+RUN apt-get install -y golang-go golang-doc
+RUN mkdir -p /go/bin && \
+  curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
 
 ENTRYPOINT ["/bin/bash"]
--- internal/config.go
+++ internal/config.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"text/template"
 
-	"github.com/google/gopacket/pfring"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
@@ -21,21 +20,23 @@ Usage:
 
 Application Options:
 {{.Flags}}
-
 Help Options:
   -h, --help                    Show this help message
 `
 
 var buf = new(bytes.Buffer)
 
-
 func init() {
 	pflag.BoolP("help", "h", false, "Show this help message")
 	pflag.BoolP("verbose", "v", false, "Verbose output (default false)")
+	pflag.BoolP("resolve_dns", "d", false, "Resolve IPs to names (default false)")
 	pflag.StringP("interface", "i", "eth0", "Interface to read packets from")
 	pflag.IntP("snaplen", "s", 65536, "Snap length (number of bytes max to read per packet)")
-	pflag.IntP("cluster", "c", -1, "If >= 0, sets the pfring cluster to this value")
-	pflag.IntP("clustertype", "t", int(pfring.ClusterPerFlow), "cluster type")
+	pflag.StringP("filter", "f", "","BPF filter for pcap")
+	pflag.IntP("connection_max_buffer", "c", 0, "Max packets to buffer for a single connection; 0 or less is infinity")
+	pflag.IntP("total_max_buffer", "t", 0, "Max packets total to buffer; 0 or less is infinity")
+	pflag.StringP("flush_after", "a", "60s", "Flush gaps in buffered packets for a connection after they hit a specific age")
+	pflag.IntP("packet_count", "p", -1, "Quit after processing this many packets, negative means infinity")
 }
 
 func Config() (*viper.Viper) {
